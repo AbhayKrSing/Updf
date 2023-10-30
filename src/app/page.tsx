32c64b0f34c2cf686f276c7b39999485cd881617
Abhay <kabhay849@gmail.com>
2023-10-31 02:34:10 +0530

    saving to continue further

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
+import { cn } from "@/lib/utils";
+import "./globals.css";
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper>
-        <div>hello</div>
+      <MaxWidthWrapper
+        className={cn(
+          "flex justify-center items-center space-x-2 mt-20 mb-12 sm:mt-16"
+        )}
+      >
+        <div className="text-sm font-semibold bg-white border shadow-md border-red-200 p-2 rounded-lg hover:border-red-500 hover:bg-red-200 transition-all">
+          <p className="">Updf is now live!</p>
+        </div>
       </MaxWidthWrapper>
     </>
   );
